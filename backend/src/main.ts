7d96a7a5708083bd61044b59a7d0fa4ef4cb0c12
mayazad <officialmayazad@gmail.com>
2026-03-02 13:41:52 +0600

    chore: allow vercel production url in cors - add by MayazAD

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
-      'mayaz-fit-one.vercel.app',
+      'fit-one-bit.vercel.app',
 
       /\.vercel\.app$/, // Allows all Vercel preview deployments
     ],
